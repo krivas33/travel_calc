e31964cf569f8556b37420108a7e147cb2ac6e54
Kevin Rivas <krivas2433@gmail.com>
2016-07-15 14:08:36 -0400

    fixed object of arrays.

--- travel.js
+++ travel.js
@@ -1,16 +1,24 @@
 var cars = [
-  model: 'Mazda',
-  mpg: 18
+  {
+    model: 'Mazda',
+    mpg: 18
+  }
 ];
 
 var trips = [
-  cost: null,
-  distance: 200,
-  map: null,
-  time: 295 // Minutes
+  {
+    cost: null,
+    distance: 200,
+    map: null,
+    time: 295 // Minutes
+  }
 ];
 
 var gasStations = [
-  price: 1.99,
-  name: 'SHEETZ'
+  {
+    price: 1.99,
+    name: 'SHEETZ'
+  }
 ];
+
+console.log(cars);
