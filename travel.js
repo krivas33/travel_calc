f3655d228fdb9a55bc601e8700d5b6af60c0ee36
Kevin Rivas <krivas2433@gmail.com>
2016-07-15 13:18:14 -0400

    Added GAS and Trip and GasStations vars

---
+++ travel.js
@@ -0,0 +1,16 @@
+var cars = [
+  mpg: 18,
+  model: 'Mazda'
+];
+
+var trips = [
+  distance: 200,
+  map: null,
+  cost: null,
+  time: 295 //Minites
+];
+
+var gasStations = [
+  name: 'SHEETZ',
+  costOfGas: 1.99
+];
